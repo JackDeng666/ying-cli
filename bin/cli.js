ae5cbf7c139480ea0c8d0863f8986498dffa86fb
邓恩泽 <1556393081@qq.com>
2021-10-06 21:02:24 +0800

    v1.0.3

---
+++ bin/cli.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+const mainFn = require('..')
+const {program} = require('commander')
+const version = require('../package.json').version
+
+// 基础用法 添加单个命令动作
+// program
+//   .command('create')
+//   .alias('crt')
+//   .description('创建项目，后面跟项目名')
+//   .action(() => {
+//     console.log('create')
+//   })
+// 多个命令保存容器中
+const actionsMap = {
+  create: {
+    alias: 'crt',
+    des: '创建项目，后面跟项目名',
+    examples: ['yingCLi create|crt <projectName>']
+  }
+}
+Object.keys(actionsMap).forEach((action) => {
+  program
+    .command(action)
+    .alias(actionsMap[action].alias)
+    .description(actionsMap[action].des)
+    .action(() => {
+      mainFn(action, process.argv.slice(3))
+    })
+})
+
+program.on('--help', () => {
+  console.log('示例：')
+  Object.keys(actionsMap).forEach((action) => {
+    actionsMap[action].examples.forEach((item) => {
+      console.log("  " + item)
+    })
+  })
+})
+
+program.version(version).parse(process.argv)
